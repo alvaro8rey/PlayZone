406cdf302f4c5149d0e5e6b0ba46c3508a72d3a4
alvaro <alvaro@macbook-alvaro.local>
2026-04-17 17:40:18 +0200

    Primer commit

---
+++ PlayZone/PlayZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayZoneApp.swift
+//  PlayZone
+//
+//  Created by alvaro on 17/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlayZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayZoneTests/PlayZoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayZoneTests.swift
+//  PlayZoneTests
+//
+//  Created by alvaro on 17/04/2026.
+//
+
+import Testing
+@testable import PlayZone
+
+struct PlayZoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
